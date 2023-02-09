811c842227bfe0f72c21c3e3a25207c06d1ae187
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-09 09:07:14 +0530

    Add activity extension functions to hide and show system bars

--- app/src/main/java/dev/anilbeesetti/nextplayer/PlayerActivity.kt
+++ app/src/main/java/dev/anilbeesetti/nextplayer/PlayerActivity.kt
@@ -1,17 +1,14 @@
 package dev.anilbeesetti.nextplayer
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import dev.anilbeesetti.nextplayer.databinding.ActivityPlayerBinding
 
 class PlayerActivity : ComponentActivity() {
@@ -40,17 +37,35 @@ class PlayerActivity : ComponentActivity() {
     }
 
     private fun initializePlayer() {
-        player = ExoPlayer.Builder(this)
-            .build()
-            .also { player ->
-                binding.playerView.player = player
-                data?.let { player.addMediaItem(MediaItem.fromUri(it)) }
-                player.playWhenReady = true
-                player.prepare()
-            }
+        player = ExoPlayer.Builder(this).build().also { player ->
+            binding.playerView.player = player
+            data?.let { player.addMediaItem(MediaItem.fromUri(it)) }
+            player.playWhenReady = true
+            player.prepare()
+        }
     }
 
     private fun releasePlayer() {
         player?.release()
+    }
+}
+
+/**
+ * Hide system bars
+ */
+fun Activity.hideSystemBars() {
+    WindowCompat.getInsetsController(window, window.decorView).apply {
+        systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        hide(WindowInsetsCompat.Type.systemBars())
+    }
+}
+
+/**
+ * Show system bars
+ */
+fun Activity.showSystemBars() {
+    WindowCompat.getInsetsController(window, window.decorView).apply {
+        systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        show(WindowInsetsCompat.Type.systemBars())
     }
 }
