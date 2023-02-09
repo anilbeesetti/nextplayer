cacddcc6cd498ab52eda8bb5c3bc5ed0927beddf
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2023-02-09 09:13:27 +0530

    Fix: Change systembars visibility on controller visibility change (#3)
    * Add activity extension functions to hide and show system bars
    
    * Add controller visibity listener to show and hide systembars

--- app/src/main/java/dev/anilbeesetti/nextplayer/PlayerActivity.kt
+++ app/src/main/java/dev/anilbeesetti/nextplayer/PlayerActivity.kt
@@ -1,17 +1,16 @@
 package dev.anilbeesetti.nextplayer
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.media3.ui.PlayerView
 import dev.anilbeesetti.nextplayer.databinding.ActivityPlayerBinding
 
 class PlayerActivity : ComponentActivity() {
@@ -40,17 +39,47 @@ class PlayerActivity : ComponentActivity() {
     }
 
     private fun initializePlayer() {
-        player = ExoPlayer.Builder(this)
-            .build()
-            .also { player ->
-                binding.playerView.player = player
-                data?.let { player.addMediaItem(MediaItem.fromUri(it)) }
-                player.playWhenReady = true
-                player.prepare()
-            }
+        player = ExoPlayer.Builder(this).build().also { player ->
+            binding.playerView.player = player
+            binding.playerView.setControllerVisibilityListener(
+                PlayerView.ControllerVisibilityListener { visibility ->
+                    when (visibility) {
+                        View.VISIBLE -> {
+                            this.showSystemBars()
+                        }
+                        View.GONE -> {
+                            this.hideSystemBars()
+                        }
+                    }
+                }
+            )
+            data?.let { player.addMediaItem(MediaItem.fromUri(it)) }
+            player.playWhenReady = true
+            player.prepare()
+        }
     }
 
     private fun releasePlayer() {
         player?.release()
+    }
+}
+
+/**
+ * Hide system bars
+ */
+fun Activity.hideSystemBars() {
+    WindowCompat.getInsetsController(window, window.decorView).apply {
+        systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        hide(WindowInsetsCompat.Type.systemBars())
+    }
+}
+
+/**
+ * Show system bars
+ */
+fun Activity.showSystemBars() {
+    WindowCompat.getInsetsController(window, window.decorView).apply {
+        systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        show(WindowInsetsCompat.Type.systemBars())
     }
 }
