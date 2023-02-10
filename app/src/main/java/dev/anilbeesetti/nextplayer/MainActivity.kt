527d7e36d35d91bf314861806e13fc55bb23c126
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-10 11:38:30 +0530

    Fix padding in lazycolumn

--- app/src/main/java/dev/anilbeesetti/nextplayer/MainActivity.kt
+++ app/src/main/java/dev/anilbeesetti/nextplayer/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -22,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import dev.anilbeesetti.nextplayer.ui.theme.NextPlayerTheme
 
 
@@ -52,7 +54,9 @@ class MainActivity : ComponentActivity() {
                                 )
                             }
                         )
-                        LazyColumn() {
+                        LazyColumn(
+                            contentPadding = PaddingValues(vertical = 10.dp)
+                        ) {
                             items(mediaList) { mediaItem ->
                                 MediaItem(
                                     media = mediaItem,
