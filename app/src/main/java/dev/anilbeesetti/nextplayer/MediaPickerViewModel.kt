fc9e4b21fd4c65ee3d02ef37a8153eaa7b86ecc4
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-12 13:32:40 +0530

    create mediapicker viewmodel

---
+++ app/src/main/java/dev/anilbeesetti/nextplayer/MediaPickerViewModel.kt
@@ -0,0 +1,8 @@
+package dev.anilbeesetti.nextplayer
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MediaPickerViewModel(): ViewModel() {
+}
