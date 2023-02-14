27c39e435c8a08b0f625edcdb738c9e6e2a17a52
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-14 15:01:31 +0530

    refactor: move videopicker to feature module

--- feature/videopicker/build.gradle.kts
+++ feature/videopicker/build.gradle.kts
@@ -1,35 +1,21 @@
 @Suppress("DSL_SCOPE_VIOLATION") // False positive
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.hilt)
     alias(libs.plugins.kotlin.kapt)
 }
 
 android {
-    namespace = "dev.anilbeesetti.nextplayer"
+    namespace = "dev.anilbeesetti.nextplayer.feature.videopicker"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "dev.anilbeesetti.nextplayer"
         minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -43,17 +29,9 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.2"
     }
-    packagingOptions {
-        resources {
-            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
-        }
-    }
 }
 
 dependencies {
-
-    implementation(project(":feature:videopicker"))
-    implementation(project(":feature:player"))
 
     // compose bom
     implementation(platform(libs.androidx.compose.bom))
@@ -61,8 +39,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.bundles.compose)
+    implementation(libs.github.skydoves.landscapist.glide)
     implementation(libs.androidx.appcompat)
     implementation(libs.google.android.material)
+
+    implementation(libs.accompanist.permissions)
 
     // hilt
     implementation(libs.hilt.android)
@@ -70,12 +51,6 @@ dependencies {
     implementation(libs.androidx.hilt.navigation.compose)
 
     testImplementation(libs.junit4)
-
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-
     androidTestImplementation(libs.androidx.test.ext)
     androidTestImplementation(libs.androidx.test.espresso.core)
-    androidTestImplementation(libs.androidx.compose.ui.test)
-    debugImplementation(libs.androidx.compose.ui.tooling.preview)
-    debugImplementation(libs.androidx.compose.ui.testManifest)
 }
