d71412e77f7cae4723a9821af750ea2eadb1b4a9
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-14 09:07:19 +0530

    lint: run ktlintFormat

--- feature/player/src/androidTest/java/dev/anilbeesetti/nextplayer/feature/player/ExampleInstrumentedTest.kt
+++ feature/player/src/androidTest/java/dev/anilbeesetti/nextplayer/feature/player/ExampleInstrumentedTest.kt
@@ -1,11 +1,10 @@
 package dev.anilbeesetti.nextplayer.feature.player
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.Assert.assertEquals
 
 /**
  * Instrumented test, which will execute on an Android device.
--- feature/player/src/test/java/dev/anilbeesetti/nextplayer/feature/player/ExampleUnitTest.kt
+++ feature/player/src/test/java/dev/anilbeesetti/nextplayer/feature/player/ExampleUnitTest.kt
@@ -1,7 +1,7 @@
 package dev.anilbeesetti.nextplayer.feature.player
 
-import org.junit.Test
 import org.junit.Assert.assertEquals
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
