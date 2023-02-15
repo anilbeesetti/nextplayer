3cfe26dc77abefaddb9f36060367576bc49f0759
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-15 14:40:46 +0530

    build: add abi split

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,16 @@ android {
             )
         }
     }
+
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
