aa8491cda14f32bc019448f01b956092bf555c39
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-17 09:05:52 +0530

    add get all video paths functionality to repository

--- core/data/src/main/java/dev/anilbeesetti/nextplayer/core/data/repository/VideoRepositoryImpl.kt
+++ core/data/src/main/java/dev/anilbeesetti/nextplayer/core/data/repository/VideoRepositoryImpl.kt
@@ -1,12 +1,17 @@
 package dev.anilbeesetti.nextplayer.core.data.repository
 
-import dev.anilbeesetti.nextplayer.core.data.util.MediaManager
+import android.net.Uri
+import dev.anilbeesetti.nextplayer.core.data.util.FileManager
 import dev.anilbeesetti.nextplayer.core.data.util.VideoItem
-import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
 class VideoRepositoryImpl @Inject constructor(
-    private val mediaManager: MediaManager
+    private val fileManager: FileManager
 ) : VideoRepository {
-    override fun getVideos(): Flow<List<VideoItem>> = mediaManager.getVideosFlow()
+    override fun getVideoItemsFlow(): Flow<List<VideoItem>> = fileManager.getVideoItemsFlow()
+
+    override fun getAllVideoPaths(): List<String> = fileManager.getAllVideosDataColumn()
+
+    override fun getPath(contentUri: Uri): String? = fileManager.getPath(contentUri)
 }
