5dc3a96038ad15a0a21a91c52b98f92bee335846
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-17 09:01:36 +0530

    add utility extensions for Uri and ContentResolver

---
+++ core/data/src/main/java/dev/anilbeesetti/nextplayer/core/data/util/UriExt.kt
@@ -0,0 +1,28 @@
+package dev.anilbeesetti.nextplayer.core.data.util
+
+import android.net.Uri
+
+/**
+ * Whether the Uri authority is ExternalStorageProvider.
+ */
+val Uri.isExternalStorageDocument: Boolean
+    get() = "com.android.externalstorage.documents" == authority
+
+/**
+ * Whether the Uri authority is DownloadsProvider.
+ */
+val Uri.isDownloadsDocument: Boolean
+    get() = "com.android.providers.downloads.documents" == authority
+
+/**
+ * Whether the Uri authority is MediaProvider.
+ */
+val Uri.isMediaDocument: Boolean
+    get() = "com.android.providers.media.documents" == authority
+
+
+/**
+ * Whether the Uri authority is Google Photos.
+ */
+val Uri.isGooglePhotosUri: Boolean
+    get() = "com.google.android.apps.photos.content" == authority
