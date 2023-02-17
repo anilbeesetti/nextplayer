510695f6258087774203489c6303d001a2ac9d57
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2023-02-17 11:16:49 +0530

    Feat: Play videos as playlist (#37)
    * feat: play videos as playlist
    
    * lint: run ktlintFomat

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/PlayerActivity.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/PlayerActivity.kt
@@ -6,21 +6,33 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.activity.ComponentActivity
+import androidx.core.net.toUri
 import androidx.core.view.WindowCompat
+import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.common.VideoSize
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
+import dagger.hilt.android.AndroidEntryPoint
+import dev.anilbeesetti.nextplayer.core.data.repository.VideoRepository
 import dev.anilbeesetti.nextplayer.feature.player.databinding.ActivityPlayerBinding
 import dev.anilbeesetti.nextplayer.feature.player.utils.hideSystemBars
 import dev.anilbeesetti.nextplayer.feature.player.utils.showSystemBars
+import java.io.File
+import javax.inject.Inject
 
+private const val TAG = "PlayerActivity"
+
+@AndroidEntryPoint
 class PlayerActivity : ComponentActivity() {
 
     private lateinit var binding: ActivityPlayerBinding
+    @Inject lateinit var videoRepository: VideoRepository
+    private var videosList: List<String> = emptyList()
+    private var data: String? = null
     private var player: Player? = null
-    private var data: Uri? = null
+    private var dataUri: Uri? = null
     private var playWhenReady = true
     private var playbackPosition = 0L
 
@@ -30,7 +42,15 @@ class PlayerActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
-        data = intent.data
+        dataUri = intent.data
+
+        dataUri?.let {
+            if (it.scheme == "content") {
+                videosList = videoRepository.getAllVideoPaths()
+                data = videoRepository.getPath(it)
+            }
+        }
+
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
@@ -60,7 +80,21 @@ class PlayerActivity : ComponentActivity() {
                     }
                 }
             )
-            data?.let { player.addMediaItem(MediaItem.fromUri(it)) }
+
+            val currentMediaItemIndex = videosList.indexOfFirst { it == data }
+            if (currentMediaItemIndex != -1) {
+                val mediaItems: MutableList<MediaItem> = mutableListOf()
+                videosList.forEach {
+                    val mediaItem = MediaItem.Builder()
+                        .setUri(File(it).toUri())
+                        .build()
+
+                    mediaItems.add(mediaItem)
+                }
+                player.setMediaItems(mediaItems, currentMediaItemIndex, C.TIME_UNSET)
+            } else {
+                dataUri?.let { player.addMediaItem(MediaItem.fromUri(it)) }
+            }
 
             player.playWhenReady = playWhenReady
             player.seekTo(playbackPosition)
