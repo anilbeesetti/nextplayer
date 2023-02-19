f94292f462f0c43f503866fc51d98713a14cdf17
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-19 16:47:22 +0530

    build: migrate to convention plugins
    Migrating to convention plugins to manage repeated gradle build configuration at a single place

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,42 @@
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "dev.anilbeesetti.nextplayer.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplicationCompose") {
+            id = "nextplayer.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("androidApplication") {
+            id = "nextplayer.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "nextplayer.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+        register("androidLibrary") {
+            id = "nextplayer.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidHilt") {
+            id = "nextplayer.android.hilt"
+            implementationClass = "AndroidHiltConventionPlugin"
+        }
+    }
+}
---
+++ build-logic/convention/src/main/kotlin/dev/anilbeesetti/nextplayer/KotlinAndroid.kt
@@ -0,0 +1,36 @@
+package dev.anilbeesetti.nextplayer
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.plugins.ExtensionAware
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+
+/**
+ * Configure base Kotlin with Android options
+ */
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *>,
+) {
+    commonExtension.apply {
+        compileSdk = 33
+
+        defaultConfig {
+            minSdk = 24
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+
+        kotlinOptions {
+            // Set JVM target to 11
+            jvmTarget = JavaVersion.VERSION_11.toString()
+        }
+    }
+}
+
+fun CommonExtension<*, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
+    (this as ExtensionAware).extensions.configure("kotlinOptions", block)
+}
