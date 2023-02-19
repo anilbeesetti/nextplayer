5e5a03f3d0b7dc1dc65be7e18cbf9eadb8e5af09
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-19 17:23:06 +0530

    lint: run ktlintFormat

--- feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/VideoPickerScreen.kt
+++ feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/VideoPickerScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.anilbeesetti.nextplayer.feature.videopicker.composables.VideoItemsPickerView
 
