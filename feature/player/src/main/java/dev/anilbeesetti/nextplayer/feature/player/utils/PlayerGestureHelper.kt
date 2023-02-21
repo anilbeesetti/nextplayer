a00e62013cc4f02326a3b3d9c0abdbf7c2b4d2f9
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-21 15:59:47 +0530

    feat: add gesture to control volume

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/utils/PlayerGestureHelper.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/utils/PlayerGestureHelper.kt
@@ -1,15 +1,25 @@
 package dev.anilbeesetti.nextplayer.feature.player.utils
 
 import android.annotation.SuppressLint
+import android.media.AudioManager
 import android.view.GestureDetector
 import android.view.MotionEvent
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.ui.PlayerView
+import dev.anilbeesetti.nextplayer.feature.player.PlayerActivity
+
+
+const val FULL_SWIPE_RANGE_SCREEN_RATIO = 0.66f
 
 @SuppressLint("ClickableViewAccessibility")
 class PlayerGestureHelper(
-    private val playerView: PlayerView
+    private val activity: PlayerActivity,
+    private val playerView: PlayerView,
+    private val audioManager: AudioManager
 ) {
+
+    private var swipeGestureValueTrackerVolume = -1f
+
 
     private val tapGestureDetector = GestureDetector(
         playerView.context,
@@ -32,11 +42,48 @@ class PlayerGestureHelper(
         }
     )
 
+
+    private val volumeAndBrightnessGestureDetector = GestureDetector(
+        playerView.context,
+        object : GestureDetector.SimpleOnGestureListener() {
+            override fun onScroll(
+                firstEvent: MotionEvent,
+                currentEvent: MotionEvent,
+                distanceX: Float,
+                distanceY: Float
+            ): Boolean {
+
+                val viewCenterX = playerView.measuredWidth / 2
+
+                val distanceFull = playerView.measuredHeight * FULL_SWIPE_RANGE_SCREEN_RATIO
+                val ratioChange = distanceY / distanceFull
+
+                if (firstEvent.x.toInt() > viewCenterX) {
+                    val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+                    if (swipeGestureValueTrackerVolume == -1f) swipeGestureValueTrackerVolume =
+                        currentVolume.toFloat()
+
+                    val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+                    val change = ratioChange * maxVolume
+                    swipeGestureValueTrackerVolume += change
+
+                    val toSet = swipeGestureValueTrackerVolume.toInt().coerceIn(0, maxVolume)
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, toSet, 0)
+
+                } else {
+                    // TODO
+                }
+                return true
+            }
+        }
+    )
+
     init {
         playerView.setOnTouchListener { _, motionEvent ->
             when (motionEvent.pointerCount) {
                 1 -> {
                     tapGestureDetector.onTouchEvent(motionEvent)
+                    volumeAndBrightnessGestureDetector.onTouchEvent(motionEvent)
                 }
                 2 -> {
                 }
