bf3630b377da358e367bb47d2eed3de3701eaa4f
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-21 08:07:20 +0530

    test: add test logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,15 @@
-
 @file:Suppress("DSL_SCOPE_VIOLATION") // False positive
+
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
 }
 
 subprojects {
@@ -18,5 +20,25 @@ subprojects {
         outputColorName.set("RED")
         ignoreFailures.set(false)
         disabledRules.add("max-line-length")
+    }
+}
+
+allprojects {
+    tasks.withType<Test>().configureEach {
+        testLogging {
+            events = setOf(
+                TestLogEvent.PASSED,
+                TestLogEvent.FAILED,
+                TestLogEvent.SKIPPED,
+                TestLogEvent.STANDARD_OUT,
+                TestLogEvent.STANDARD_ERROR
+            )
+
+            exceptionFormat = TestExceptionFormat.FULL
+            showExceptions = true
+            showCauses = true
+            showStackTraces = true
+        }
+        ignoreFailures = true
     }
 }
