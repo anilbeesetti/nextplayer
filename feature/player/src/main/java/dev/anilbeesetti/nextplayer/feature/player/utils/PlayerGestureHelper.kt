6c28d36eb94e3ae4afae53ad6e936588dfc8b341
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-21 15:01:23 +0530

    feat: add play and pause functionality on double tap event

---
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/utils/PlayerGestureHelper.kt
@@ -0,0 +1,49 @@
+package dev.anilbeesetti.nextplayer.feature.player.utils
+
+import android.annotation.SuppressLint
+import android.view.GestureDetector
+import android.view.MotionEvent
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.ui.PlayerView
+
+
+@SuppressLint("ClickableViewAccessibility")
+class PlayerGestureHelper(
+    private val playerView: PlayerView,
+) {
+
+    private val tapGestureDetector = GestureDetector(
+        playerView.context,
+        @UnstableApi object : GestureDetector.SimpleOnGestureListener() {
+            override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
+                playerView.apply {
+                    if (!isControllerFullyVisible) showController() else hideController()
+                }
+                return true
+            }
+
+            override fun onDoubleTap(e: MotionEvent): Boolean {
+                if (playerView.player?.isPlaying == true) {
+                    playerView.player?.pause()
+                } else {
+                    playerView.player?.play()
+                }
+                return true
+            }
+        }
+    )
+
+    init {
+        playerView.setOnTouchListener { _, motionEvent ->
+            when (motionEvent.pointerCount) {
+                1 -> {
+                    tapGestureDetector.onTouchEvent(motionEvent)
+                }
+                2 -> {
+
+                }
+            }
+            true
+        }
+    }
+}
