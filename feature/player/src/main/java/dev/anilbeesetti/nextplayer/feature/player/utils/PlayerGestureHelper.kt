780d5d55ab89c7c94e788ff198b3d4b804318267
anilbeesetti <anilbeesetti10@gmail.com>
2023-02-24 14:00:47 +0530

    feat: hide volume and brightness indicators after input finished

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/utils/PlayerGestureHelper.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/utils/PlayerGestureHelper.kt
@@ -23,6 +23,9 @@ class PlayerGestureHelper(
     private var swipeGestureVolumeTrackerValue = -1f
     private var swipeGestureBrightnessTrackerValue = -1f
 
+    private var swipeGestureVolumeOpen = false
+    private var swipeGestureBrightnessOpen = false
+
     private val tapGestureDetector = GestureDetector(
         playerView.context,
         @UnstableApi object : GestureDetector.SimpleOnGestureListener() {
@@ -81,13 +84,15 @@ class PlayerGestureHelper(
                         0
                     )
 
+                    val volumePercentage = (swipeGestureVolumeTrackerValue / maxVolume.toFloat()).times(100).toInt()
+                    val volumeText = "$volumePercentage%"
+
                     activity.binding.gestureVolumeLayout.visibility = View.VISIBLE
                     activity.binding.gestureVolumeProgressBar.max = maxVolume
                     activity.binding.gestureVolumeProgressBar.progress = swipeGestureVolumeTrackerValue.toInt()
-                    val volumePercentage = (swipeGestureVolumeTrackerValue / maxVolume.toFloat()).times(100).toInt()
-                    val volumeText = "$volumePercentage%"
                     activity.binding.gestureVolumeText.text = volumeText
 
+                    swipeGestureVolumeOpen = true
                 } else {
                     val brightnessRange = BRIGHTNESS_OVERRIDE_OFF..BRIGHTNESS_OVERRIDE_FULL
                     if (swipeGestureBrightnessTrackerValue == -1f) {
@@ -107,20 +112,52 @@ class PlayerGestureHelper(
                     layoutParams.screenBrightness = swipeGestureBrightnessTrackerValue
                     activity.window.attributes = layoutParams
 
-                    // fix a bug which makes the action bar reappear after changing the brightness
+                    // fixes a bug which makes the action bar reappear after changing the brightness
                     activity.swipeToShowStatusBars()
+
+                    val brightnessPercentage = (layoutParams.screenBrightness / BRIGHTNESS_OVERRIDE_FULL).times(100).toInt()
+                    val brightnessText = "$brightnessPercentage%"
 
                     activity.binding.gestureBrightnessLayout.visibility = View.VISIBLE
                     activity.binding.gestureBrightnessProgressBar.max = BRIGHTNESS_OVERRIDE_FULL.times(100).toInt()
                     activity.binding.gestureBrightnessProgressBar.progress = layoutParams.screenBrightness.times(100).toInt()
-                    val brightnessPercentage = (layoutParams.screenBrightness / BRIGHTNESS_OVERRIDE_FULL).times(100).toInt()
-                    val brightnessText = "$brightnessPercentage%"
                     activity.binding.gestureBrightnessText.text = brightnessText
+
+                    swipeGestureBrightnessOpen = true
                 }
                 return true
             }
         }
     )
+
+    private val hideVolumeGestureIndicator = Runnable {
+        activity.binding.gestureVolumeLayout.visibility = View.GONE
+    }
+
+    private val hideBrightnessGestureIndicator = Runnable {
+        activity.binding.gestureBrightnessLayout.visibility = View.GONE
+    }
+
+    private fun releaseAction(event: MotionEvent) {
+        if (event.action == MotionEvent.ACTION_UP) {
+            // hide the volume indicator
+            activity.binding.gestureVolumeLayout.apply {
+                if (visibility == View.VISIBLE) {
+                    removeCallbacks(hideVolumeGestureIndicator)
+                    postDelayed(hideVolumeGestureIndicator, HIDE_DELAY_MILLIS)
+                    swipeGestureVolumeOpen = false
+                }
+            }
+            // hide the brightness indicator
+            activity.binding.gestureBrightnessLayout.apply {
+                if (visibility == View.VISIBLE) {
+                    removeCallbacks(hideBrightnessGestureIndicator)
+                    postDelayed(hideBrightnessGestureIndicator, HIDE_DELAY_MILLIS)
+                    swipeGestureBrightnessOpen = false
+                }
+            }
+        }
+    }
 
     init {
         playerView.setOnTouchListener { _, motionEvent ->
@@ -132,11 +169,13 @@ class PlayerGestureHelper(
                 2 -> {
                 }
             }
+            releaseAction(motionEvent)
             true
         }
     }
 
     companion object {
         const val FULL_SWIPE_RANGE_SCREEN_RATIO = 0.66f
+        const val HIDE_DELAY_MILLIS = 1000L
     }
 }
