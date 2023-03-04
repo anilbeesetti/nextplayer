bffed37e12a59ceffa75b2237583c2a9f0512767
anilbeesetti <anilbeesetti10@gmail.com>
2023-03-04 16:39:21 +0530

    refactor: move appbar to core ui module and refactor composables

--- feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/VideoPickerScreen.kt
+++ feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/VideoPickerScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -45,6 +46,8 @@ internal fun VideoPickerScreen(
     uiState: VideoPickerUiState,
     onVideoItemClick: (uri: Uri) -> Unit
 ) {
+
+
     Box(
         modifier = Modifier
             .fillMaxSize(),
