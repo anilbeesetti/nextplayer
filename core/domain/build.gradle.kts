a3825b607f45beadd8a2098d1ddda836c3967371
anilbeesetti <anilbeesetti10@gmail.com>
2023-03-04 09:35:07 +0530

    add domain module

---
+++ core/domain/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("nextplayer.android.library")
+    id("nextplayer.android.hilt")
+}
+
+android {
+    namespace = "dev.anilbeesetti.nextplayer.core.domain"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.kotlinx.coroutines.android)
+
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
