45c922f67a968c74ba1e0012b442687d74510413
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2023-04-07 08:41:22 +0000

    Refactor: Use scaffold instead of column in `VideoPickerScreen` (#152)

--- app/src/main/java/dev/anilbeesetti/nextplayer/MainActivity.kt
+++ app/src/main/java/dev/anilbeesetti/nextplayer/MainActivity.kt
@@ -10,20 +10,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Dashboard
-import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
@@ -46,7 +37,7 @@ import dev.anilbeesetti.nextplayer.feature.videopicker.VideoPickerScreen
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -79,36 +70,9 @@ class MainActivity : ComponentActivity() {
                     }
 
                     if (storagePermissionState.status.isGranted) {
-                        Column {
-                            var showMenu by remember {
-                                mutableStateOf(false)
-                            }
-
-                            NextPlayerMainTopAppBar(
-                                titleRes = R.string.app_name,
-                                navigationIcon = {
-                                    IconButton(onClick = { /*TODO*/ }) {
-                                        Icon(
-                                            imageVector = Icons.Rounded.Settings,
-                                            contentDescription = "Settings"
-                                        )
-                                    }
-                                },
-                                actions = {
-                                    IconButton(onClick = { showMenu = true }) {
-                                        Icon(
-                                            imageVector = Icons.Rounded.Dashboard,
-                                            contentDescription = "Dashboard"
-                                        )
-                                    }
-                                }
-                            )
-                            VideoPickerScreen(
-                                onVideoItemClick = { startPlayerActivity(it) },
-                                showMenu = showMenu,
-                                showMenuDialog = { showMenu = it }
-                            )
-                        }
+                        VideoPickerScreen(
+                            onVideoItemClick = { startPlayerActivity(it) }
+                        )
                     } else {
                         PermissionScreen(
                             permission = storagePermissionState.permission,
--- feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/VideoPickerScreen.kt
+++ feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/VideoPickerScreen.kt
@@ -5,14 +5,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Title
+import androidx.compose.material.icons.rounded.Dashboard
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -28,6 +38,7 @@ import dev.anilbeesetti.nextplayer.core.datastore.SortOrder
 import dev.anilbeesetti.nextplayer.core.ui.DayNightPreview
 import dev.anilbeesetti.nextplayer.core.ui.DevicePreviews
 import dev.anilbeesetti.nextplayer.core.ui.VideoPickerPreviewParameterProvider
+import dev.anilbeesetti.nextplayer.core.ui.components.NextPlayerMainTopAppBar
 import dev.anilbeesetti.nextplayer.core.ui.theme.NextPlayerTheme
 import dev.anilbeesetti.nextplayer.feature.videopicker.composables.MenuDialog
 import dev.anilbeesetti.nextplayer.feature.videopicker.composables.TextIconToggleButton
@@ -38,8 +49,6 @@ const val CIRCULAR_PROGRESS_INDICATOR_TEST_TAG = "circularProgressIndicator"
 @Composable
 fun VideoPickerScreen(
     viewModel: VideoPickerViewModel = hiltViewModel(),
-    showMenu: Boolean,
-    showMenuDialog: (Boolean) -> Unit = {},
     onVideoItemClick: (uri: Uri) -> Unit
 ) {
     val videosState by viewModel.videoItems.collectAsStateWithLifecycle()
@@ -47,56 +56,80 @@ fun VideoPickerScreen(
 
     VideoPickerScreen(
         videosState = videosState,
-        showDialog = showMenu,
         preferences = preferences,
         onVideoItemClick = onVideoItemClick,
-        showMenuDialog = showMenuDialog,
         updatePreferences = viewModel::updateMenu
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun VideoPickerScreen(
     videosState: VideosState,
-    showDialog: Boolean = false,
     preferences: AppPreferences,
     onVideoItemClick: (uri: Uri) -> Unit = {},
-    showMenuDialog: (Boolean) -> Unit = {},
     updatePreferences: (SortBy, SortOrder) -> Unit = { _, _ -> }
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        when (videosState) {
-            is VideosState.Loading -> {
-                CircularProgressIndicator(
-                    modifier = Modifier.testTag(CIRCULAR_PROGRESS_INDICATOR_TEST_TAG)
-                )
-            }
-            is VideosState.Success -> {
-                if (videosState.videos.isEmpty()) {
-                    Column {
-                        Text(
-                            text = stringResource(id = R.string.no_videos_found),
-                            style = MaterialTheme.typography.titleLarge
+    var showMenu by rememberSaveable { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            NextPlayerMainTopAppBar(
+                titleRes = R.string.app_name,
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Rounded.Settings,
+                            contentDescription = stringResource(id = R.string.settings)
                         )
                     }
-                } else {
-                    VideoItemsPickerView(
-                        videos = videosState.videos,
-                        onVideoItemClick = onVideoItemClick
+                },
+                actions = {
+                    IconButton(onClick = { showMenu = true }) {
+                        Icon(
+                            imageVector = Icons.Rounded.Dashboard,
+                            contentDescription = stringResource(id = R.string.menu)
+                        )
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            when (videosState) {
+                is VideosState.Loading -> {
+                    CircularProgressIndicator(
+                        modifier = Modifier.testTag(CIRCULAR_PROGRESS_INDICATOR_TEST_TAG)
                     )
                 }
+                is VideosState.Success -> {
+                    if (videosState.videos.isEmpty()) {
+                        Column {
+                            Text(
+                                text = stringResource(id = R.string.no_videos_found),
+                                style = MaterialTheme.typography.titleLarge
+                            )
+                        }
+                    } else {
+                        VideoItemsPickerView(
+                            videos = videosState.videos,
+                            onVideoItemClick = onVideoItemClick
+                        )
+                    }
+                }
             }
-        }
-        if (showDialog) {
-            MenuDialog(
-                preferences = preferences,
-                showMenuDialog = showMenuDialog,
-                update = updatePreferences
-            )
+            if (showMenu) {
+                MenuDialog(
+                    preferences = preferences,
+                    showMenuDialog = { showMenu = it },
+                    update = updatePreferences
+                )
+            }
         }
     }
 }
@@ -115,8 +148,7 @@ fun VideoPickerScreenPreview(
                         videos = videos
                     ),
                     preferences = AppPreferences(),
-                    onVideoItemClick = {},
-                    showMenuDialog = {}
+                    onVideoItemClick = {}
                 )
             }
         }
@@ -145,8 +177,7 @@ fun VideoPickerNoVideosFoundPreview() {
                     videos = emptyList()
                 ),
                 preferences = AppPreferences(),
-                onVideoItemClick = {},
-                showMenuDialog = {}
+                onVideoItemClick = {}
             )
         }
     }
@@ -160,8 +191,7 @@ fun VideoPickerLoadingPreview() {
             VideoPickerScreen(
                 videosState = VideosState.Loading,
                 preferences = AppPreferences(),
-                onVideoItemClick = {},
-                showMenuDialog = {}
+                onVideoItemClick = {}
             )
         }
     }
