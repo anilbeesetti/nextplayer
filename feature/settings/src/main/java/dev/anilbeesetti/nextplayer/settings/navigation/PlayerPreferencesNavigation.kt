6c663dd827a15c274053ff3c56e06d342ae1cb35
anilbeesetti <anilbeesetti10@gmail.com>
2023-04-08 14:26:46 +0530

    refactor: move player preferences files to player package

--- feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/navigation/PlayerPreferencesNavigation.kt
+++ feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/navigation/PlayerPreferencesNavigation.kt
@@ -4,7 +4,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import dev.anilbeesetti.nextplayer.settings.screens.PlayerPreferencesScreen
+import dev.anilbeesetti.nextplayer.settings.screens.player.PlayerPreferencesScreen
 
 const val playerPreferencesNavigationRoute = "player_preferences_route"
 
