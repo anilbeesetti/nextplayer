3764dcebe776b388c2616b4c4be260df74e3f17e
anilbeesetti <anilbeesetti10@gmail.com>
2023-04-08 10:30:53 +0530

    refactor: create a composable `RadioTextButton`

--- feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/screens/PlayerPreferencesScreen.kt
+++ feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/screens/PlayerPreferencesScreen.kt
@@ -2,12 +2,9 @@ package dev.anilbeesetti.nextplayer.settings.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
@@ -15,24 +12,21 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.anilbeesetti.nextplayer.core.datastore.Resume
 import dev.anilbeesetti.nextplayer.core.ui.components.CancelButton
 import dev.anilbeesetti.nextplayer.core.ui.components.NextDialog
 import dev.anilbeesetti.nextplayer.core.ui.components.NextDialogDefaults
+import dev.anilbeesetti.nextplayer.core.ui.components.RadioTextButton
 import dev.anilbeesetti.nextplayer.feature.settings.R
 import dev.anilbeesetti.nextplayer.settings.composables.ClickablePreferenceItem
 
@@ -98,31 +92,16 @@ fun PlayerPreferencesScreen(
                             modifier = Modifier.selectableGroup()
                         ) {
                             Resume.values().forEach {
-                                Row(
+                                RadioTextButton(
+                                    text = it.name,
+                                    selected = (it == preferences.resume),
+                                    onClick = {
+                                        viewModel.updateResume(it)
+                                        viewModel.onEvent(PlayerPreferencesEvent.ResumeDialog(false))
+                                    },
                                     modifier = Modifier
-                                        .fillMaxWidth()
-                                        .selectable(
-                                            selected = (it == preferences.resume),
-                                            onClick = {
-                                                viewModel.updateResume(it)
-                                                viewModel.onEvent(
-                                                    PlayerPreferencesEvent.ResumeDialog(false)
-                                                )
-                                            },
-                                            role = Role.RadioButton
-                                        )
-                                        .padding(horizontal = NextDialogDefaults.dialogPadding),
-                                    verticalAlignment = Alignment.CenterVertically
-                                ) {
-                                    RadioButton(
-                                        selected = (it == preferences.resume),
-                                        onClick = null
-                                    )
-                                    Text(
-                                        text = it.name,
-                                        modifier = Modifier.padding(16.dp)
-                                    )
-                                }
+                                        .padding(horizontal = NextDialogDefaults.dialogPadding)
+                                )
                             }
                         }
                     },
