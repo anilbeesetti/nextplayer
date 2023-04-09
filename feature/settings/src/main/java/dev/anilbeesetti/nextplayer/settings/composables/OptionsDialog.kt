6b3a2918f8eada88c788d42538ba314b12d542b2
anilbeesetti <anilbeesetti10@gmail.com>
2023-04-09 16:05:35 +0530

    fix: dialog not scrollable

--- feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/composables/OptionsDialog.kt
+++ feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/composables/OptionsDialog.kt
@@ -1,7 +1,10 @@
 package dev.anilbeesetti.nextplayer.settings.composables
 
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,7 +27,12 @@ fun OptionsDialog(
         },
         content = {
             Column(
-                modifier = Modifier.selectableGroup()
+                modifier = Modifier
+                    .scrollable(
+                        state = rememberScrollState(),
+                        orientation = Orientation.Vertical
+                    )
+                    .selectableGroup()
             ) {
                 options()
             }
