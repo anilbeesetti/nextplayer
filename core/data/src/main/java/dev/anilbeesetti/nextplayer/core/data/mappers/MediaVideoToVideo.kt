649f36ba807c65909a79a60bc3634e10131560a5
anilbeesetti <anilbeesetti10@gmail.com>
2023-04-12 17:00:02 +0530

    remove unused mapper function

--- core/data/src/main/java/dev/anilbeesetti/nextplayer/core/data/mappers/MediaVideoToVideo.kt
+++ core/data/src/main/java/dev/anilbeesetti/nextplayer/core/data/mappers/MediaVideoToVideo.kt
@@ -1,8 +1,6 @@
 package dev.anilbeesetti.nextplayer.core.data.mappers
 
-import dev.anilbeesetti.nextplayer.core.data.models.Folder
 import dev.anilbeesetti.nextplayer.core.data.models.Video
-import dev.anilbeesetti.nextplayer.core.media.model.MediaFolder
 import dev.anilbeesetti.nextplayer.core.media.model.MediaVideo
 import java.io.File
 
@@ -18,14 +16,5 @@ fun MediaVideo.toVideo(): Video {
         width = width,
         height = height,
         size = size
-    )
-}
-
-fun MediaFolder.toFolder(): Folder {
-    return Folder(
-        name = name,
-        path = path,
-        mediaSize = media.sumOf { it.size },
-        mediaCount = media.size
     )
 }
