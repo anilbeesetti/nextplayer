a8e68e1125fa508e93c3142fbc77cb6fece0cc06
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2023-04-16 09:42:24 +0000

    Fix: Remove extra navigation bars padding (#195)
    * fix: unneccessary naviagtion bars padding
    
    * use material theme parent

--- feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/composables/MediaContent.kt
+++ feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/composables/MediaContent.kt
@@ -1,6 +1,10 @@
 package dev.anilbeesetti.nextplayer.feature.videopicker.composables
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -8,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import dev.anilbeesetti.nextplayer.core.data.models.Folder
 import dev.anilbeesetti.nextplayer.core.data.models.Video
 import dev.anilbeesetti.nextplayer.core.ui.R
@@ -25,6 +30,7 @@ fun MediaContent(
                 modifier = Modifier.testTag(CIRCULAR_PROGRESS_INDICATOR_TEST_TAG)
             )
         }
+
         is MediaState.Success<*> -> {
             if (state.data.isEmpty()) {
                 Column {
@@ -34,16 +40,23 @@ fun MediaContent(
                     )
                 }
             } else {
-                MediaPicker(list = state.data) { data ->
-                    when (data) {
-                        is Folder -> FolderItem(
-                            folder = data,
-                            onClick = { onMediaClick(data.path) }
-                        )
-                        is Video -> VideoItem(
-                            video = data,
-                            onClick = { onMediaClick(data.uriString) }
-                        )
+                LazyColumn(
+                    contentPadding = PaddingValues(vertical = 10.dp),
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    items(state.data) { data ->
+                        when (data) {
+                            is Folder -> FolderItem(
+                                folder = data,
+                                onClick = { onMediaClick(data.path) }
+                            )
+
+                            is Video -> VideoItem(
+                                video = data,
+                                onClick = { onMediaClick(data.uriString) }
+                            )
+                        }
                     }
                 }
             }
