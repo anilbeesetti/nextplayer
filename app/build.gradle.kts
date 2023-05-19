8ea609fbf14a679965d4567154110478f893f1f4
anilbeesetti <anilbeesetti10@gmail.com>
2023-05-19 09:34:49 +0530

    build: change versionCode to 1 and versionName to 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "dev.anilbeesetti.nextplayer"
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
     }
 
     buildFeatures {
