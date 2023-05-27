453b692f90eacd4c89973c3b56cc19298993b0f0
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2023-05-27 02:16:24 +0000

    Fix: Remember player selected tracks and speed (#280)
    * fix: remember selections
    
    * refactor: rename playbackPosition to align with other variables
    
    * lint: run ktlintFormat

--- core/data/src/main/java/dev/anilbeesetti/nextplayer/core/data/repository/VideoRepository.kt
+++ core/data/src/main/java/dev/anilbeesetti/nextplayer/core/data/repository/VideoRepository.kt
@@ -25,8 +25,7 @@ interface VideoRepository {
         position: Long,
         audioTrackIndex: Int?,
         subtitleTrackIndex: Int?,
-        playbackSpeed: Float,
-        rememberSelections: Boolean
+        playbackSpeed: Float
     )
 
     /**
