8a66e60e2a510c37a3a43f81338b0a49179b95de
anilbeesetti <anilbeesetti10@gmail.com>
2023-05-30 14:21:59 +0530

    fix: test case errors

--- feature/videopicker/src/androidTest/java/dev/anilbeesetti/nextplayer/feature/videopicker/MediaPickerScreenTest.kt
+++ feature/videopicker/src/androidTest/java/dev/anilbeesetti/nextplayer/feature/videopicker/MediaPickerScreenTest.kt
@@ -188,12 +188,14 @@ val foldersTestData = listOf(
         name = "Folder 1",
         path = "/storage/emulated/0/DCIM/Camera/Folder 1",
         mediaCount = 1,
-        mediaSize = 1000
+        mediaSize = 1000,
+        isExcluded = false
     ),
     Folder(
         name = "Folder 2",
         path = "/storage/emulated/0/DCIM/Camera/Folder 2",
         mediaCount = 2,
-        mediaSize = 2000
+        mediaSize = 2000,
+        isExcluded = false
     )
 )
