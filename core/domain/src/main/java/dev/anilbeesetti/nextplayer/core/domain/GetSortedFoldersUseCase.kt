e8f7a347755ce0e97b052c79abd3115c38b500a7
anilbeesetti <anilbeesetti10@gmail.com>
2023-05-30 09:27:51 +0530

    feat: filter out videos that are from excluded folders

--- core/domain/src/main/java/dev/anilbeesetti/nextplayer/core/domain/GetSortedFoldersUseCase.kt
+++ core/domain/src/main/java/dev/anilbeesetti/nextplayer/core/domain/GetSortedFoldersUseCase.kt
@@ -2,40 +2,29 @@ package dev.anilbeesetti.nextplayer.core.domain
 
 import dev.anilbeesetti.nextplayer.core.common.Dispatcher
 import dev.anilbeesetti.nextplayer.core.common.NextDispatchers
-import dev.anilbeesetti.nextplayer.core.common.extensions.prettyName
 import dev.anilbeesetti.nextplayer.core.data.repository.PreferencesRepository
-import dev.anilbeesetti.nextplayer.core.data.repository.VideoRepository
 import dev.anilbeesetti.nextplayer.core.model.Folder
 import dev.anilbeesetti.nextplayer.core.model.SortBy
 import dev.anilbeesetti.nextplayer.core.model.SortOrder
-import java.io.File
-import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
 
 class GetSortedFoldersUseCase @Inject constructor(
-    private val videoRepository: VideoRepository,
+    private val getAllFoldersUseCase: GetAllFoldersUseCase,
     private val preferencesRepository: PreferencesRepository,
     @Dispatcher(NextDispatchers.Default) private val defaultDispatcher: CoroutineDispatcher
 ) {
 
     operator fun invoke(): Flow<List<Folder>> {
         return combine(
-            videoRepository.getVideosFlow(),
+            getAllFoldersUseCase.invoke(),
             preferencesRepository.appPrefsFlow
-        ) { videoItems, preferences ->
+        ) { allFolders, preferences ->
 
-            val folders = videoItems.groupBy { File(it.path).parentFile!! }
-                .map { (file, videos) ->
-                    Folder(
-                        path = file.path,
-                        name = file.prettyName,
-                        mediaCount = videos.size,
-                        mediaSize = videos.sumOf { it.size }
-                    )
-                }
+            val folders = allFolders.filter { !it.isExcluded }
 
             when (preferences.sortOrder) {
                 SortOrder.ASCENDING -> {
