bdb315cf03235d19dfb7332ccabfaf22eefee974
anilbeesetti <anilbeesetti10@gmail.com>
2023-06-04 15:25:28 +0530

    add more screen orientation options

--- core/model/src/main/java/dev/anilbeesetti/nextplayer/core/model/ScreenOrientation.kt
+++ core/model/src/main/java/dev/anilbeesetti/nextplayer/core/model/ScreenOrientation.kt
@@ -3,6 +3,8 @@ package dev.anilbeesetti.nextplayer.core.model
 enum class ScreenOrientation {
     AUTOMATIC,
     LANDSCAPE,
+    LANDSCAPE_REVERSE,
+    LANDSCAPE_AUTO,
     PORTRAIT,
     SYSTEM_DEFAULT,
     VIDEO_ORIENTATION
--- feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/extensions/ScreenOrientation.kt
+++ feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/extensions/ScreenOrientation.kt
@@ -10,6 +10,8 @@ fun ScreenOrientation.name(): String {
     val stringRes = when (this) {
         ScreenOrientation.AUTOMATIC -> R.string.automatic
         ScreenOrientation.LANDSCAPE -> R.string.landscape
+        ScreenOrientation.LANDSCAPE_REVERSE -> R.string.landscape_reverse
+        ScreenOrientation.LANDSCAPE_AUTO -> R.string.landscape_auto
         ScreenOrientation.PORTRAIT -> R.string.portrait
         ScreenOrientation.SYSTEM_DEFAULT -> R.string.system_default
         ScreenOrientation.VIDEO_ORIENTATION -> R.string.video_orientation
