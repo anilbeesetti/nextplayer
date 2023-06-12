63d7f9574fc49094e82e2868bd1bf8974c48f578
anilbeesetti <anilbeesetti10@gmail.com>
2023-06-12 10:06:06 +0530

    lint: run ktlintFormat

--- feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/composables/OptionsDialog.kt
+++ feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/composables/OptionsDialog.kt
@@ -1,20 +1,15 @@
 package dev.anilbeesetti.nextplayer.settings.composables
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import dev.anilbeesetti.nextplayer.core.ui.components.CancelButton
-import dev.anilbeesetti.nextplayer.core.ui.components.DoneButton
 import dev.anilbeesetti.nextplayer.core.ui.components.NextDialog
 
 @Composable
@@ -38,28 +33,5 @@ fun OptionsDialog(
         },
         dismissButton = { CancelButton(onClick = onDismissClick) },
         confirmButton = { }
-    )
-}
-
-@Composable
-fun InputDialog(
-    title: String,
-    description: String,
-    onDismissClick: () -> Unit,
-    onDoneClick: () -> Unit,
-    content: @Composable () -> Unit
-) {
-    NextDialog(
-        onDismissRequest = { /*TODO*/ },
-        title = {
-            Text(text = title)
-        },
-        content = {
-            Text(text = description)
-            Spacer(modifier = Modifier.height(20.dp))
-            content()
-        },
-        confirmButton = { DoneButton(onClick = onDoneClick) },
-        dismissButton = { CancelButton(onClick = onDismissClick) }
     )
 }
