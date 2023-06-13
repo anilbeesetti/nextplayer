612e3e1903ac1a87c7e31d8c6d79101fa40bffe8
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2023-06-13 10:47:36 +0530

    Refactor: Extend `DefaultRenderersFactory` instead of creating one from scratch (#356)
    * refactor: extend defaultRenderersFactory instead of creating new renderers factory
    
    * refactor: rename renderers factory

---
+++ nextlib/ffcodecs/src/main/java/dev/anilbeesetti/libs/ffcodecs/NextRenderersFactory.java
@@ -0,0 +1,60 @@
+package dev.anilbeesetti.libs.ffcodecs;
+
+import android.content.Context;
+import android.os.Handler;
+
+import androidx.media3.common.util.Log;
+import androidx.media3.common.util.UnstableApi;
+import androidx.media3.exoplayer.DefaultRenderersFactory;
+import androidx.media3.exoplayer.Renderer;
+import androidx.media3.exoplayer.audio.AudioRendererEventListener;
+import androidx.media3.exoplayer.audio.AudioSink;
+import androidx.media3.exoplayer.mediacodec.MediaCodecSelector;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+
+@UnstableApi
+public class NextRenderersFactory extends DefaultRenderersFactory {
+
+    /**
+     * @param context A {@link Context}.
+     */
+    public NextRenderersFactory(Context context) {
+        super(context);
+    }
+    
+    private static final String TAG = "NextRenderersFactory";
+
+    @Override
+    protected void buildAudioRenderers(Context context, int extensionRendererMode, MediaCodecSelector mediaCodecSelector, boolean enableDecoderFallback, AudioSink audioSink, Handler eventHandler, AudioRendererEventListener eventListener, ArrayList<Renderer> out) {
+        super.buildAudioRenderers(context, extensionRendererMode, mediaCodecSelector, enableDecoderFallback, audioSink, eventHandler, eventListener, out);
+
+        if (extensionRendererMode == EXTENSION_RENDERER_MODE_OFF) {
+            return;
+        }
+        int extensionRendererIndex = out.size();
+        if (extensionRendererMode == EXTENSION_RENDERER_MODE_PREFER) {
+            extensionRendererIndex--;
+        }
+
+        try {
+            // Full class names used for constructor args so the LINT rule triggers if any of them move.
+            Class<?> clazz = Class.forName("dev.anilbeesetti.libs.ffcodecs.FfmpegAudioRenderer");
+            Constructor<?> constructor =
+                    clazz.getConstructor(
+                            android.os.Handler.class,
+                            androidx.media3.exoplayer.audio.AudioRendererEventListener.class,
+                            androidx.media3.exoplayer.audio.AudioSink.class);
+            Renderer renderer =
+                    (Renderer) constructor.newInstance(eventHandler, eventListener, audioSink);
+            out.add(extensionRendererIndex++, renderer);
+            Log.i(TAG, "Loaded FfmpegAudioRenderer.");
+        } catch (ClassNotFoundException e) {
+            // Expected if the app was built without the extension.
+        } catch (Exception e) {
+            // The extension is present, but instantiation failed.
+            throw new RuntimeException("Error instantiating FFmpeg extension", e);
+        }
+    }
+}
