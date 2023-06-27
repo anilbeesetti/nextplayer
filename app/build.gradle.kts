3eb1f2d9e464a692e2ab34a93003adf18c5c7b2a
anilbeesetti <anilbeesetti10@gmail.com>
2023-06-27 08:07:28 +0530

    fix: default preferences are used in player on cold start

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    implementation(project(":core:common"))
     implementation(project(":core:data"))
     implementation(project(":core:ui"))
     implementation(project(":core:model"))
