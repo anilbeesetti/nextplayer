100f14a546181eb8919c156904f954f373c419c0
anilbeesetti <anilbeesetti10@gmail.com>
2023-06-29 09:11:40 +0530

    build: bump versionCode and version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
 
     defaultConfig {
         applicationId = "dev.anilbeesetti.nextplayer"
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
     }
 
     buildFeatures {
