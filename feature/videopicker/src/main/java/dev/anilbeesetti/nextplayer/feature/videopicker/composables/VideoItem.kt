f1a8ef3fdb105befe8f411f10fc17324b23b7fa7
anilbeesetti <anilbeesetti10@gmail.com>
2023-07-07 09:57:36 +0530

    refactor: videoItem composable to use material3 listItem

--- feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/composables/VideoItem.kt
+++ feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/composables/VideoItem.kt
@@ -1,20 +1,18 @@
 package dev.anilbeesetti.nextplayer.feature.videopicker.composables
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,6 +20,7 @@ import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.layout.ContentScale
@@ -43,38 +42,25 @@ fun VideoItem(
 ) {
     val haptic = LocalHapticFeedback.current
 
-    Box(
-        modifier = Modifier.combinedClickable(
-            onClick = onClick,
-            onLongClick = { haptic.performHapticFeedback(HapticFeedbackType.LongPress) }
-        )
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(horizontal = 12.dp, vertical = 8.dp)
-        ) {
-            Box {
-                Surface(
-                    color = MaterialTheme.colorScheme.surfaceColorAtElevation(1.dp),
-                    shape = MaterialTheme.shapes.small,
-                    modifier = Modifier
-                        .widthIn(max = 420.dp)
-                        .fillMaxWidth(0.45f)
-                        .aspectRatio(16f / 10f),
-                    content = {
-                        if (video.uriString.isNotEmpty()) {
-                            GlideImage(
-                                imageModel = { video.uriString },
-                                imageOptions = ImageOptions(
-                                    contentScale = ContentScale.Crop,
-                                    alignment = Alignment.Center
-                                )
-                            )
-                        }
-                    }
-                )
+    ListItem(
+        leadingContent = {
+            Box(
+                modifier = Modifier
+                    .clip(MaterialTheme.shapes.small)
+                    .background(MaterialTheme.colorScheme.surfaceColorAtElevation(1.dp),)
+                    .widthIn(max = 400.dp)
+                    .fillMaxWidth(0.5f)
+                    .aspectRatio(16f / 10f)
+            ) {
+                if (video.uriString.isNotEmpty()) {
+                    GlideImage(
+                        imageModel = { video.uriString },
+                        imageOptions = ImageOptions(
+                            contentScale = ContentScale.Crop,
+                            alignment = Alignment.Center
+                        )
+                    )
+                }
                 InfoChip(
                     text = video.formattedDuration,
                     modifier = Modifier
@@ -85,46 +71,45 @@ fun VideoItem(
                     shape = MaterialTheme.shapes.small.copy(CornerSize(3.dp))
                 )
             }
-            Column(
-                modifier = Modifier
-                    .padding(start = 12.dp)
-                    .fillMaxWidth()
+        },
+        headlineContent = {
+            Text(
+                text = video.displayName,
+                maxLines = 2,
+                style = MaterialTheme.typography.titleMedium,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        supportingContent = {
+            Text(
+                text = video.path,
+                maxLines = 2,
+                style = MaterialTheme.typography.bodySmall,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.padding(vertical = 2.dp)
+            )
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(5.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(
-                    text = video.displayName,
-                    maxLines = 2,
-                    style = MaterialTheme.typography.titleMedium,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    overflow = TextOverflow.Ellipsis
+                InfoChip(
+                    text = video.formattedFileSize,
+                    modifier = Modifier.padding(vertical = 5.dp)
                 )
-                Spacer(modifier = Modifier.height(3.dp))
-                Text(
-                    text = video.path,
-                    maxLines = 2,
-                    style = MaterialTheme.typography.bodySmall,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Spacer(modifier = Modifier.height(2.dp))
-                FlowRow(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(5.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
+                if (video.width > 0 && video.height > 0) {
                     InfoChip(
-                        text = video.formattedFileSize,
+                        text = "${video.width} x ${video.height}",
                         modifier = Modifier.padding(vertical = 5.dp)
                     )
-                    if (video.width > 0 && video.height > 0) {
-                        InfoChip(
-                            text = "${video.width} x ${video.height}",
-                            modifier = Modifier.padding(vertical = 5.dp)
-                        )
-                    }
                 }
             }
-        }
-    }
+        },
+        modifier = modifier.combinedClickable(
+            onClick = onClick,
+            onLongClick = { haptic.performHapticFeedback(HapticFeedbackType.LongPress) }
+        )
+    )
 }
 
 @DayNightPreview
