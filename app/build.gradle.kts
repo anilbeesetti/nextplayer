1f1eb2096711ed60250e9c8bd3b6c9eac7ea66ad
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2023-07-22 21:20:58 +0530

    build: bump version to 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
 
     defaultConfig {
         applicationId = "dev.anilbeesetti.nextplayer"
-        versionCode = 5
-        versionName = "0.4.0"
+        versionCode = 6
+        versionName = "0.5.0"
     }
 
     buildFeatures {
