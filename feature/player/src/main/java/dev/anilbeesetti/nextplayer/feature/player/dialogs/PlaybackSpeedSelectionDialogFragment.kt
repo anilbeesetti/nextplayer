e9db27ccdf13022f29b73331ec9d1abad335f64f
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2023-08-04 10:08:35 +0530

    add playback speed dialog

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/dialogs/PlaybackSpeedSelectionDialogFragment.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/dialogs/PlaybackSpeedSelectionDialogFragment.kt
@@ -4,29 +4,49 @@ import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import dev.anilbeesetti.nextplayer.feature.player.databinding.PlaybackSpeedBinding
 import dev.anilbeesetti.nextplayer.core.ui.R as coreUiR
 
 class PlaybackSpeedSelectionDialogFragment(
     private val currentSpeed: Float,
     private val onChange: (Float) -> Unit
-//    private val player: Player
 ) : DialogFragment() {
 
+    lateinit var binding: PlaybackSpeedBinding
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val speedTexts = listOf("0.5x", "0.75x", "1x", "1.25x", "1.5x", "1.75x", "2x")
-        val speedNumbers = listOf(0.5f, 0.75f, 1f, 1.25f, 1.5f, 1.75f, 2f)
+        binding = PlaybackSpeedBinding.inflate(layoutInflater)
 
         return activity?.let { activity ->
+
+            binding.apply {
+                speed.value = currentSpeed
+                speedText.text = currentSpeed.toString()
+                speed.addOnChangeListener { _, _, _ ->
+                    val newSpeed = String.format("%.1f", speed.value).toFloat()
+                    onChange(newSpeed)
+                    speedText.text = newSpeed.toString()
+                }
+                resetSpeed.setOnClickListener {
+                    speed.value = 1.0f
+                }
+                incSpeed.setOnClickListener {
+                    if (speed.value < 4.0f) {
+                        speed.value += 0.1f
+                    }
+                }
+                decSpeed.setOnClickListener {
+                    if (speed.value > 0.2f) {
+                        speed.value -= 0.1f
+                    }
+                }
+            }
+
+
             val builder = MaterialAlertDialogBuilder(activity)
             builder.setTitle(getString(coreUiR.string.select_playback_speed))
-                .setSingleChoiceItems(
-                    speedTexts.toTypedArray(),
-                    speedNumbers.indexOf(currentSpeed)
-                ) { dialog, which ->
-                    onChange(speedNumbers[which])
-                    dialog.dismiss()
-                }
-            builder.create()
+                .setView(binding.root)
+                .create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
 }
