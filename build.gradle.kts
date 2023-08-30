673a4c7eb332fa0347e10903df8bc33865ee11ef
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2023-08-30 12:54:08 +0530

    Refactor: Migrate room from kapt to ksp (#550)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.ktlint) apply false
 }
 
