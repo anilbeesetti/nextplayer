51321a4a3ad8f39cdff133cb83172faee084e04b
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2023-09-07 15:18:38 +0530

    refactor: migrate hilt to ksp and remove kapt (#582)

--- build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
@@ -9,16 +9,14 @@ class AndroidHiltConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("dagger.hilt.android.plugin")
-                // KAPT must go last to avoid build warnings.
-                // See: https://stackoverflow.com/questions/70550883/warning-the-following-options-were-not-recognized-by-any-processor-dagger-f
-                apply("org.jetbrains.kotlin.kapt")
+                apply("com.google.devtools.ksp")
             }
 
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
             dependencies {
                 "implementation"(libs.findLibrary("hilt.android").get())
-                "kapt"(libs.findLibrary("hilt.compiler").get())
-                "kaptAndroidTest"(libs.findLibrary("hilt.compiler").get())
+                "ksp"(libs.findLibrary("hilt.compiler").get())
+                "kspAndroidTest"(libs.findLibrary("hilt.compiler").get())
             }
 
         }
