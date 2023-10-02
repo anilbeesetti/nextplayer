16dfcf959aa577d441e0381d1c4064224c7b4907
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2023-10-02 08:54:44 +0530

    fix: loudness enhancer initialization crash on some devices

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/PlayerActivity.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/PlayerActivity.kt
@@ -60,7 +60,6 @@ import dev.anilbeesetti.nextplayer.core.model.DecoderPriority
 import dev.anilbeesetti.nextplayer.core.model.ScreenOrientation
 import dev.anilbeesetti.nextplayer.core.model.ThemeConfig
 import dev.anilbeesetti.nextplayer.core.model.VideoZoom
-import dev.anilbeesetti.nextplayer.core.ui.R as coreUiR
 import dev.anilbeesetti.nextplayer.feature.player.databinding.ActivityPlayerBinding
 import dev.anilbeesetti.nextplayer.feature.player.dialogs.PlaybackSpeedControlsDialogFragment
 import dev.anilbeesetti.nextplayer.feature.player.dialogs.TrackSelectionDialogFragment
@@ -92,13 +91,14 @@ import dev.anilbeesetti.nextplayer.feature.player.utils.PlaylistManager
 import dev.anilbeesetti.nextplayer.feature.player.utils.VolumeManager
 import dev.anilbeesetti.nextplayer.feature.player.utils.toMillis
 import io.github.anilbeesetti.nextlib.media3ext.ffdecoder.NextRenderersFactory
-import java.nio.charset.Charset
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import timber.log.Timber
+import java.nio.charset.Charset
+import dev.anilbeesetti.nextplayer.core.ui.R as coreUiR
 
 @SuppressLint("UnsafeOptInUsageError")
 @AndroidEntryPoint
@@ -317,15 +317,15 @@ class PlayerActivity : AppCompatActivity() {
             .setHandleAudioBecomingNoisy(playerPreferences.pauseOnHeadsetDisconnect)
             .build()
 
-        if (player.canAdvertiseSession()) {
-            try {
+        try {
+            if (player.canAdvertiseSession()) {
                 mediaSession = MediaSession.Builder(this, player).build()
-            } catch (e: IllegalStateException) {
-                e.printStackTrace()
             }
+            loudnessEnhancer = LoudnessEnhancer(player.audioSessionId)
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
         player.addListener(playbackStateListener)
-        loudnessEnhancer = LoudnessEnhancer(player.audioSessionId)
         volumeManager.loudnessEnhancer = loudnessEnhancer
     }
 
