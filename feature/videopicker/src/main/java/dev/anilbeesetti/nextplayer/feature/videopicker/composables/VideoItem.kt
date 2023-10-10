7e38b0b1c980873b361877b20dd482f546733887
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2023-10-10 07:45:01 +0530

    show ffmpeg thumbnail in VideoItem

--- feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/composables/VideoItem.kt
+++ feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/composables/VideoItem.kt
@@ -66,9 +66,9 @@ fun VideoItem(
                         .align(Alignment.Center)
                         .fillMaxSize(0.5f)
                 )
-                if (video.uriString.isNotEmpty() && preferences.showThumbnailField) {
+                if (!video.thumbnailPath.isNullOrEmpty() && preferences.showThumbnailField) {
                     GlideImage(
-                        imageModel = { video.uriString },
+                        imageModel = { video.thumbnailPath },
                         imageOptions = ImageOptions(
                             contentScale = ContentScale.Crop,
                             alignment = Alignment.Center
