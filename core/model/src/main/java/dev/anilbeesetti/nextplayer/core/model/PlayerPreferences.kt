f1ac2e5dbbcef4c816fc44da7fe84b7631b76f52
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2023-12-30 09:18:47 +0530

    Feat: Add volume boost preference (#805)

--- core/model/src/main/java/dev/anilbeesetti/nextplayer/core/model/PlayerPreferences.kt
+++ core/model/src/main/java/dev/anilbeesetti/nextplayer/core/model/PlayerPreferences.kt
@@ -30,6 +30,7 @@ data class PlayerPreferences(
     val pauseOnHeadsetDisconnect: Boolean = true,
     val requireAudioFocus: Boolean = true,
     val showSystemVolumePanel: Boolean = true,
+    val shouldUseVolumeBoost: Boolean = false,
 
     // Subtitle Preferences
     val useSystemCaptionStyle: Boolean = false,
