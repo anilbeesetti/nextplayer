52fbe2b55a3d023d0d1c78797e79c06c3acafdc8
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2024-03-30 09:44:41 +0530

    fix divider not showing in preferences item

--- core/ui/src/main/java/dev/anilbeesetti/nextplayer/core/ui/components/PreferenceSwitchWithDivider.kt
+++ core/ui/src/main/java/dev/anilbeesetti/nextplayer/core/ui/components/PreferenceSwitchWithDivider.kt
@@ -3,7 +3,6 @@ package dev.anilbeesetti.nextplayer.core.ui.components
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,8 +11,6 @@ import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -48,7 +45,7 @@ fun PreferenceSwitchWithDivider(
                 VerticalDivider(
                     modifier = Modifier
                         .padding(end = 12.dp)
-                        .height(IntrinsicSize.Max),
+                        .height(40.dp),
                     color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f)
                 )
                 NextSwitch(
