93c6a522694ecd4e750297a5450725849bcb9ab0
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2024-05-04 10:30:40 +0530

    remove convention plugin usage in core:domain build.gradle.kts

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,10 +1,26 @@
 plugins {
-    id("nextplayer.android.library")
-    id("nextplayer.android.hilt")
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "dev.anilbeesetti.nextplayer.core.domain"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get().toInt())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get().toInt())
+    }
+
+    kotlinOptions {
+        jvmTarget = libs.versions.android.jvm.get()
+    }
 }
 
 dependencies {
@@ -15,6 +31,11 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.kotlinx.coroutines.android)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    kspAndroidTest(libs.hilt.compiler)
 
     testImplementation(libs.junit4)
     testImplementation(libs.kotlinx.coroutines.test)
