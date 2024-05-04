a114221ad6a0732f90552b7d67ac39de82bf570e
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2024-05-04 10:11:21 +0530

    remove convention plugin usage in core:database build.gradle.kts

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,11 +1,26 @@
 plugins {
-    id("nextplayer.android.library")
-    id("nextplayer.android.hilt")
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "dev.anilbeesetti.nextplayer.core.database"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get().toInt())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get().toInt())
+    }
+
+    kotlinOptions {
+        jvmTarget = libs.versions.android.jvm.get()
+    }
 
     ksp {
         arg("room.schemaLocation", "$projectDir/schemas")
@@ -15,6 +30,11 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    kspAndroidTest(libs.hilt.compiler)
 
     // Room
     implementation(libs.androidx.room.runtime)
