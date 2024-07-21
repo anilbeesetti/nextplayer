7daf39bf46b6181f50e6d0e29765c08e6e8d1867
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2024-07-21 12:37:46 +0530

    remember video zoom value between player sessions

--- core/data/src/main/java/dev/anilbeesetti/nextplayer/core/data/repository/MediaRepository.kt
+++ core/data/src/main/java/dev/anilbeesetti/nextplayer/core/data/repository/MediaRepository.kt
@@ -10,6 +10,14 @@ interface MediaRepository {
     fun getVideosFlow(): Flow<List<Video>>
     fun getVideosFlowFromFolderPath(folderPath: String): Flow<List<Video>>
     fun getFoldersFlow(): Flow<List<Folder>>
-    suspend fun saveVideoState(uri: String, position: Long, audioTrackIndex: Int?, subtitleTrackIndex: Int?, playbackSpeed: Float?, externalSubs: List<Uri>)
+    suspend fun saveVideoState(
+        uri: String,
+        position: Long,
+        audioTrackIndex: Int?,
+        subtitleTrackIndex: Int?,
+        playbackSpeed: Float?,
+        externalSubs: List<Uri>,
+        videoScale: Float,
+    )
     suspend fun getVideoState(uri: String): VideoState?
 }
