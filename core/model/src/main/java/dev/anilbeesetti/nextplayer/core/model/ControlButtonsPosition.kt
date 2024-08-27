380f4e4754826be3f8f2dfcad6ad3d6f3bc0c202
Andrii <39765531+AndreVero@users.noreply.github.com>
2024-08-27 07:29:36 +0100

    Fix ktlint issues

--- core/model/src/main/java/dev/anilbeesetti/nextplayer/core/model/ControlButtonsPosition.kt
+++ core/model/src/main/java/dev/anilbeesetti/nextplayer/core/model/ControlButtonsPosition.kt
@@ -2,5 +2,5 @@ package dev.anilbeesetti.nextplayer.core.model
 
 enum class ControlButtonsPosition {
     LEFT_BOTTOM_CORNER,
-    RIGHT_BOTTOM_CORNER
+    RIGHT_BOTTOM_CORNER,
 }
--- feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/extensions/ControlButtonsPosition.kt
+++ feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/extensions/ControlButtonsPosition.kt
@@ -6,7 +6,7 @@ import dev.anilbeesetti.nextplayer.core.model.ControlButtonsPosition
 import dev.anilbeesetti.nextplayer.core.ui.R
 
 @Composable
-fun ControlButtonsPosition.name() : String {
+fun ControlButtonsPosition.name(): String {
     val stringRes = when (this) {
         ControlButtonsPosition.LEFT_BOTTOM_CORNER -> R.string.left_bottom_corner
         ControlButtonsPosition.RIGHT_BOTTOM_CORNER -> R.string.right_bottom_corner
