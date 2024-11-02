7e389fb7563146c7731d6024cc831ddb4665f223
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2024-11-02 20:57:25 +0530

    fix crash on clicking on the notification

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/PlayerActivity.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/PlayerActivity.kt
@@ -306,7 +306,7 @@ class PlayerActivity : AppCompatActivity() {
                 addListener(playbackStateListener)
                 volumeManager.loudnessEnhancer = loudnessEnhancer
 
-                if (intent.data.toString() != currentMediaItem?.mediaId) {
+                if (intent.data != null && intent.data.toString() != currentMediaItem?.mediaId) {
                     playVideo(uri = viewModel.currentMediaItem?.localConfiguration?.uri ?: intent.data!!)
                 } else {
                     playWhenReady = viewModel.playWhenReady
