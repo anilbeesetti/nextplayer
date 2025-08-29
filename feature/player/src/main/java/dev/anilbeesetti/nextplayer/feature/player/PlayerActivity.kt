3bfa37c8724f4ecec9d385c3b5b5ecc4c010c384
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2025-08-29 15:57:39 +0530

    fix player black screen issue

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/PlayerActivity.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/PlayerActivity.kt
@@ -315,8 +315,7 @@ class PlayerActivity : AppCompatActivity() {
         playerApi = PlayerApi(this)
 
         onBackPressedDispatcher.addCallback {
-            finish()
-            mediaController?.stopPlayerSession()
+            finishAndStopPlayerSession()
         }
     }
 
@@ -834,11 +833,7 @@ class PlayerActivity : AppCompatActivity() {
             when (playbackState) {
                 Player.STATE_ENDED -> {
                     isPlaybackFinished = mediaController?.playbackState == Player.STATE_ENDED
-                    finish()
-                }
-
-                Player.STATE_IDLE -> {
-                    isPlaybackFinished = mediaController?.playbackState == Player.STATE_ENDED
+                    finishAndStopPlayerSession()
                 }
 
                 Player.STATE_READY -> {
@@ -848,6 +843,15 @@ class PlayerActivity : AppCompatActivity() {
                 }
 
                 else -> {}
+            }
+        }
+
+        override fun onPlayWhenReadyChanged(playWhenReady: Boolean, reason: Int) {
+            super.onPlayWhenReadyChanged(playWhenReady, reason)
+
+            if (reason == Player.PLAY_WHEN_READY_CHANGE_REASON_END_OF_MEDIA_ITEM) {
+                isPlaybackFinished = true
+                finishAndStopPlayerSession()
             }
         }
     }
@@ -1154,6 +1158,11 @@ class PlayerActivity : AppCompatActivity() {
             delay(HIDE_DELAY_MILLIS)
             binding.infoLayout.visibility = View.GONE
         }
+    }
+
+    private fun finishAndStopPlayerSession() {
+        finish()
+        mediaController?.stopPlayerSession()
     }
 
     companion object {
