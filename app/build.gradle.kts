ea4a4a88046f7849aff23e0f34c7111e0d93f321
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2025-10-03 16:11:02 +0530

    Bump versionCode and versionName to 41 and 0.14.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         applicationId = "dev.anilbeesetti.nextplayer"
-        versionCode = 40
-        versionName = "0.14.0"
+        versionCode = 41
+        versionName = "0.14.1"
     }
 
     buildFeatures {
