ba73414b3df33338078579f2f355a99f4e21cebe
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2026-02-19 08:06:26 +0530

    move all player gesture preferences to separate screen

---
+++ feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/screens/gesture/GesturePreferencesViewModel.kt
@@ -0,0 +1,177 @@
+package dev.anilbeesetti.nextplayer.settings.screens.gesture
+
+import androidx.compose.runtime.Stable
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.anilbeesetti.nextplayer.core.common.extensions.round
+import dev.anilbeesetti.nextplayer.core.data.repository.PreferencesRepository
+import dev.anilbeesetti.nextplayer.core.model.DoubleTapGesture
+import dev.anilbeesetti.nextplayer.core.model.PlayerPreferences
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class GesturePreferencesViewModel @Inject constructor(
+    private val preferencesRepository: PreferencesRepository,
+) : ViewModel() {
+
+    private val uiStateInternal = MutableStateFlow(
+        GesturePreferencesUiState(
+            preferences = preferencesRepository.playerPreferences.value,
+        ),
+    )
+    val uiState = uiStateInternal.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            preferencesRepository.playerPreferences.collect { preferences ->
+                uiStateInternal.update { it.copy(preferences = preferences) }
+            }
+        }
+    }
+
+    fun onEvent(event: GesturePreferencesUiEvent) {
+        when (event) {
+            is GesturePreferencesUiEvent.ShowDialog -> showDialog(event.value)
+            is GesturePreferencesUiEvent.UpdateDoubleTapGesture -> updateDoubleTapGesture(event.gesture)
+            GesturePreferencesUiEvent.ToggleUseLongPressControls -> toggleUseLongPressControls()
+            GesturePreferencesUiEvent.ToggleDoubleTapGesture -> toggleDoubleTapGesture()
+            GesturePreferencesUiEvent.ToggleEnableBrightnessSwipeGesture -> toggleEnableBrightnessSwipeGesture()
+            GesturePreferencesUiEvent.ToggleEnableVolumeSwipeGesture -> toggleEnableVolumeSwipeGesture()
+            GesturePreferencesUiEvent.ToggleUseSeekControls -> toggleUseSeekControls()
+            GesturePreferencesUiEvent.ToggleUseZoomControls -> toggleUseZoomControls()
+            GesturePreferencesUiEvent.ToggleEnablePanGesture -> toggleEnablePanGesture()
+            is GesturePreferencesUiEvent.UpdateLongPressControlsSpeed -> updateLongPressControlsSpeed(event.value)
+            is GesturePreferencesUiEvent.UpdateSeekIncrement -> updateSeekIncrement(event.value)
+            is GesturePreferencesUiEvent.UpdateSeekSensitivity -> updateSeekSensitivity(event.value)
+        }
+    }
+
+    private fun showDialog(value: GesturePreferenceDialog?) {
+        uiStateInternal.update {
+            it.copy(showDialog = value)
+        }
+    }
+
+    private fun updateDoubleTapGesture(gesture: DoubleTapGesture) {
+        viewModelScope.launch {
+            preferencesRepository.updatePlayerPreferences {
+                it.copy(doubleTapGesture = gesture)
+            }
+        }
+    }
+
+    private fun toggleUseLongPressControls() {
+        viewModelScope.launch {
+            preferencesRepository.updatePlayerPreferences {
+                it.copy(useLongPressControls = !it.useLongPressControls)
+            }
+        }
+    }
+
+    private fun toggleDoubleTapGesture() {
+        viewModelScope.launch {
+            preferencesRepository.updatePlayerPreferences {
+                it.copy(
+                    doubleTapGesture = if (it.doubleTapGesture == DoubleTapGesture.NONE) {
+                        DoubleTapGesture.FAST_FORWARD_AND_REWIND
+                    } else {
+                        DoubleTapGesture.NONE
+                    },
+                )
+            }
+        }
+    }
+
+    private fun toggleEnableBrightnessSwipeGesture() {
+        viewModelScope.launch {
+            preferencesRepository.updatePlayerPreferences {
+                it.copy(enableBrightnessSwipeGesture = !it.enableBrightnessSwipeGesture)
+            }
+        }
+    }
+
+    private fun toggleEnableVolumeSwipeGesture() {
+        viewModelScope.launch {
+            preferencesRepository.updatePlayerPreferences {
+                it.copy(enableVolumeSwipeGesture = !it.enableVolumeSwipeGesture)
+            }
+        }
+    }
+
+    private fun toggleUseSeekControls() {
+        viewModelScope.launch {
+            preferencesRepository.updatePlayerPreferences {
+                it.copy(useSeekControls = !it.useSeekControls)
+            }
+        }
+    }
+
+    private fun toggleUseZoomControls() {
+        viewModelScope.launch {
+            preferencesRepository.updatePlayerPreferences {
+                it.copy(useZoomControls = !it.useZoomControls)
+            }
+        }
+    }
+
+    private fun toggleEnablePanGesture() {
+        viewModelScope.launch {
+            preferencesRepository.updatePlayerPreferences {
+                it.copy(enablePanGesture = !it.enablePanGesture)
+            }
+        }
+    }
+
+    private fun updateLongPressControlsSpeed(value: Float) {
+        viewModelScope.launch {
+            preferencesRepository.updatePlayerPreferences { it.copy(longPressControlsSpeed = value) }
+        }
+    }
+
+    private fun updateSeekIncrement(value: Int) {
+        viewModelScope.launch {
+            preferencesRepository.updatePlayerPreferences {
+                it.copy(seekIncrement = value)
+            }
+        }
+    }
+
+    private fun updateSeekSensitivity(value: Float) {
+        viewModelScope.launch {
+            preferencesRepository.updatePlayerPreferences {
+                it.copy(seekSensitivity = value.round(1))
+            }
+        }
+    }
+}
+
+@Stable
+data class GesturePreferencesUiState(
+    val showDialog: GesturePreferenceDialog? = null,
+    val preferences: PlayerPreferences = PlayerPreferences(),
+)
+
+sealed interface GesturePreferenceDialog {
+    data object DoubleTapDialog : GesturePreferenceDialog
+    data object LongPressControlsSpeedDialog : GesturePreferenceDialog
+}
+
+sealed interface GesturePreferencesUiEvent {
+    data class ShowDialog(val value: GesturePreferenceDialog?) : GesturePreferencesUiEvent
+    data class UpdateDoubleTapGesture(val gesture: DoubleTapGesture) : GesturePreferencesUiEvent
+    data object ToggleUseLongPressControls : GesturePreferencesUiEvent
+    data object ToggleDoubleTapGesture : GesturePreferencesUiEvent
+    data object ToggleEnableBrightnessSwipeGesture : GesturePreferencesUiEvent
+    data object ToggleEnableVolumeSwipeGesture : GesturePreferencesUiEvent
+    data object ToggleUseSeekControls : GesturePreferencesUiEvent
+    data object ToggleUseZoomControls : GesturePreferencesUiEvent
+    data object ToggleEnablePanGesture : GesturePreferencesUiEvent
+    data class UpdateLongPressControlsSpeed(val value: Float) : GesturePreferencesUiEvent
+    data class UpdateSeekIncrement(val value: Int) : GesturePreferencesUiEvent
+    data class UpdateSeekSensitivity(val value: Float) : GesturePreferencesUiEvent
+}
--- feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/screens/player/PlayerPreferencesScreen.kt
+++ feature/settings/src/main/java/dev/anilbeesetti/nextplayer/settings/screens/player/PlayerPreferencesScreen.kt
@@ -3,7 +3,6 @@ package dev.anilbeesetti.nextplayer.settings.screens.player
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -16,33 +15,23 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Slider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import dev.anilbeesetti.nextplayer.core.common.extensions.round
 import dev.anilbeesetti.nextplayer.core.model.ControlButtonsPosition
-import dev.anilbeesetti.nextplayer.core.model.DoubleTapGesture
 import dev.anilbeesetti.nextplayer.core.model.PlayerPreferences
 import dev.anilbeesetti.nextplayer.core.model.Resume
 import dev.anilbeesetti.nextplayer.core.model.ScreenOrientation
 import dev.anilbeesetti.nextplayer.core.ui.R
 import dev.anilbeesetti.nextplayer.core.ui.components.ClickablePreferenceItem
 import dev.anilbeesetti.nextplayer.core.ui.components.ListSectionTitle
-import dev.anilbeesetti.nextplayer.core.ui.components.NextDialogWithDoneAndCancelButtons
 import dev.anilbeesetti.nextplayer.core.ui.components.NextTopAppBar
 import dev.anilbeesetti.nextplayer.core.ui.components.PreferenceSlider
 import dev.anilbeesetti.nextplayer.core.ui.components.PreferenceSwitch
-import dev.anilbeesetti.nextplayer.core.ui.components.PreferenceSwitchWithDivider
 import dev.anilbeesetti.nextplayer.core.ui.components.RadioTextButton
 import dev.anilbeesetti.nextplayer.core.ui.designsystem.NextIcons
 import dev.anilbeesetti.nextplayer.core.ui.preview.DayNightPreview
@@ -94,97 +83,6 @@ private fun PlayerPreferencesContent(
                 .padding(innerPadding)
                 .padding(horizontal = 16.dp),
         ) {
-            ListSectionTitle(text = stringResource(id = R.string.gestures))
-            Column(
-                verticalArrangement = Arrangement.spacedBy(ListItemDefaults.SegmentedGap),
-            ) {
-                PreferenceSwitch(
-                    title = stringResource(id = R.string.seek_gesture),
-                    description = stringResource(id = R.string.seek_gesture_description),
-                    icon = NextIcons.SwipeHorizontal,
-                    isChecked = uiState.preferences.useSeekControls,
-                    onClick = { onEvent(PlayerPreferencesUiEvent.ToggleUseSeekControls) },
-                    isFirstItem = true
-                )
-                PreferenceSwitch(
-                    title = stringResource(id = R.string.brightness_gesture),
-                    description = stringResource(id = R.string.brightness_gesture_description),
-                    icon = NextIcons.SwipeVertical,
-                    isChecked = uiState.preferences.enableBrightnessSwipeGesture,
-                    onClick = { onEvent(PlayerPreferencesUiEvent.ToggleEnableBrightnessSwipeGesture) },
-                )
-                PreferenceSwitch(
-                    title = stringResource(id = R.string.volume_gesture),
-                    description = stringResource(id = R.string.volume_gesture_description),
-                    icon = NextIcons.SwipeVertical,
-                    isChecked = uiState.preferences.enableVolumeSwipeGesture,
-                    onClick = { onEvent(PlayerPreferencesUiEvent.ToggleEnableVolumeSwipeGesture) },
-                )
-                PreferenceSwitch(
-                    title = stringResource(id = R.string.zoom_gesture),
-                    description = stringResource(id = R.string.zoom_gesture_description),
-                    icon = NextIcons.Pinch,
-                    isChecked = uiState.preferences.useZoomControls,
-                    onClick = { onEvent(PlayerPreferencesUiEvent.ToggleUseZoomControls) },
-                )
-                PreferenceSwitch(
-                    title = stringResource(id = R.string.pan_gesture),
-                    description = stringResource(id = R.string.pan_gesture_description),
-                    icon = NextIcons.Pan,
-                    enabled = uiState.preferences.useZoomControls,
-                    isChecked = uiState.preferences.enablePanGesture,
-                    onClick = { onEvent(PlayerPreferencesUiEvent.ToggleEnablePanGesture) },
-                )
-                PreferenceSwitchWithDivider(
-                    title = stringResource(id = R.string.double_tap),
-                    description = stringResource(id = R.string.double_tap_description),
-                    icon = NextIcons.DoubleTap,
-                    isChecked = (uiState.preferences.doubleTapGesture != DoubleTapGesture.NONE),
-                    onChecked = { onEvent(PlayerPreferencesUiEvent.ToggleDoubleTapGesture) },
-                    onClick = { onEvent(PlayerPreferencesUiEvent.ShowDialog(PlayerPreferenceDialog.DoubleTapDialog)) },
-                )
-                PreferenceSwitchWithDivider(
-                    title = stringResource(id = R.string.long_press_gesture),
-                    description = stringResource(id = R.string.long_press_gesture_desc, uiState.preferences.longPressControlsSpeed),
-                    icon = NextIcons.Tap,
-                    isChecked = uiState.preferences.useLongPressControls,
-                    onChecked = { onEvent(PlayerPreferencesUiEvent.ToggleUseLongPressControls) },
-                    onClick = { onEvent(PlayerPreferencesUiEvent.ShowDialog(PlayerPreferenceDialog.LongPressControlsSpeedDialog)) },
-                )
-                PreferenceSlider(
-                    title = stringResource(R.string.seek_increment),
-                    description = stringResource(R.string.seconds, uiState.preferences.seekIncrement),
-                    icon = NextIcons.Replay,
-                    value = uiState.preferences.seekIncrement.toFloat(),
-                    valueRange = 1.0f..60.0f,
-                    onValueChange = { onEvent(PlayerPreferencesUiEvent.UpdateSeekIncrement(it.toInt())) },
-                    trailingContent = {
-                        FilledIconButton(onClick = { onEvent(PlayerPreferencesUiEvent.UpdateSeekIncrement(PlayerPreferences.DEFAULT_SEEK_INCREMENT)) }) {
-                            Icon(
-                                imageVector = NextIcons.History,
-                                contentDescription = stringResource(id = R.string.reset_seek_increment),
-                            )
-                        }
-                    },
-                )
-                PreferenceSlider(
-                    title = stringResource(R.string.seek_sensitivity),
-                    description = uiState.preferences.seekSensitivity.toString(),
-                    icon = NextIcons.FastForward,
-                    value = uiState.preferences.seekSensitivity,
-                    valueRange = 0.1f..2.0f,
-                    onValueChange = { onEvent(PlayerPreferencesUiEvent.UpdateSeekSensitivity(it)) },
-                    isLastItem = true,
-                    trailingContent = {
-                        FilledIconButton(onClick = { onEvent(PlayerPreferencesUiEvent.UpdateSeekSensitivity(PlayerPreferences.DEFAULT_SEEK_SENSITIVITY)) }) {
-                            Icon(
-                                imageVector = NextIcons.History,
-                                contentDescription = stringResource(id = R.string.reset_seek_sentivity),
-                            )
-                        }
-                    },
-                )
-            }
             ListSectionTitle(text = stringResource(id = R.string.interface_name))
             Column(
                 verticalArrangement = Arrangement.spacedBy(ListItemDefaults.SegmentedGap),
@@ -318,24 +216,6 @@ private fun PlayerPreferencesContent(
                     }
                 }
 
-                PlayerPreferenceDialog.DoubleTapDialog -> {
-                    OptionsDialog(
-                        text = stringResource(id = R.string.double_tap),
-                        onDismissClick = { onEvent(PlayerPreferencesUiEvent.ShowDialog(null)) },
-                    ) {
-                        items(DoubleTapGesture.entries.toTypedArray()) {
-                            RadioTextButton(
-                                text = it.name(),
-                                selected = (it == uiState.preferences.doubleTapGesture),
-                                onClick = {
-                                    onEvent(PlayerPreferencesUiEvent.UpdateDoubleTapGesture(it))
-                                    onEvent(PlayerPreferencesUiEvent.ShowDialog(null))
-                                },
-                            )
-                        }
-                    }
-                }
-
                 PlayerPreferenceDialog.PlayerScreenOrientationDialog -> {
                     OptionsDialog(
                         text = stringResource(id = R.string.player_screen_orientation),
@@ -370,36 +250,6 @@ private fun PlayerPreferencesContent(
                             )
                         }
                     }
-                }
-
-                PlayerPreferenceDialog.LongPressControlsSpeedDialog -> {
-                    var longPressControlsSpeed by remember {
-                        mutableFloatStateOf(uiState.preferences.longPressControlsSpeed)
-                    }
-
-                    NextDialogWithDoneAndCancelButtons(
-                        title = stringResource(R.string.long_press_gesture),
-                        onDoneClick = {
-                            onEvent(PlayerPreferencesUiEvent.UpdateLongPressControlsSpeed(longPressControlsSpeed))
-                            onEvent(PlayerPreferencesUiEvent.ShowDialog(null))
-                        },
-                        onDismissClick = { onEvent(PlayerPreferencesUiEvent.ShowDialog(null)) },
-                        content = {
-                            Text(
-                                text = "$longPressControlsSpeed",
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(vertical = 20.dp),
-                                textAlign = TextAlign.Center,
-                                style = MaterialTheme.typography.titleMedium,
-                            )
-                            Slider(
-                                value = longPressControlsSpeed,
-                                onValueChange = { longPressControlsSpeed = it.round(1) },
-                                valueRange = 0.2f..4.0f,
-                            )
-                        },
-                    )
                 }
             }
         }
