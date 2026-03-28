09fb57f1c2ab23bbb52124e197dd808fe5149e45
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2026-03-28 15:28:25 +0530

    handle negative player duration during metadata update

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/service/PlayerService.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/service/PlayerService.kt
@@ -280,7 +280,7 @@ class PlayerService : MediaSessionService() {
             // Update the media metadata duration so that it will be used later in position discontinuity handling
             player.replaceMediaItem(
                 player.currentMediaItemIndex,
-                currentMediaItem.copy(durationMs = player.duration)
+                currentMediaItem.copy(durationMs = if (player.duration < 0) C.TIME_UNSET else player.duration)
             )
         }
 
