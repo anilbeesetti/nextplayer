b57573a8c6a24dc34428b21beea3ce642ffb6a39
Narayan Chetri <107901367+NarayanChetri@users.noreply.github.com>
2026-05-17 21:51:39 +0530

    2c

--- core/media/src/main/java/dev/anilbeesetti/nextplayer/core/media/services/MediaService.kt
+++ core/media/src/main/java/dev/anilbeesetti/nextplayer/core/media/services/MediaService.kt
@@ -11,6 +11,17 @@ interface MediaService {
     suspend fun shareMedia(uris: List<Uri>)
     suspend fun hideVideos(uris: List<Uri>): Boolean
 
+    /**
+     * Moves files from the vault back to the given [destinationDir] (defaults to Movies).
+     * [filenames] are relative to the vault directory.
+     */
+    suspend fun unhideVideos(filenames: List<String>, destinationDir: String? = null): Boolean
+
+    /**
+     * Returns the list of filenames currently stored in the vault directory.
+     */
+    fun listVaultFiles(): List<String>
+
     companion object {
         fun willSystemAsksForDeleteConfirmation(): Boolean {
             return Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
