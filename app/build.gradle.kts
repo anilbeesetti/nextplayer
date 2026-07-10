ed036c89adf545735e3b1c0405f34c4859e286ba
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2026-07-10 19:45:56 +0530

    Increment versionCode from 61 to 62

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         applicationId = "dev.anilbeesetti.nextplayer"
-        versionCode = 61
+        versionCode = 62
         versionName = "0.17.0"
     }
 
