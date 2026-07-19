c3084e91ff3569063b5a44ce33df6e3897a2e353
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2026-07-19 09:27:40 +0530

    Bump version code to 70

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         applicationId = "dev.anilbeesetti.nextplayer"
-        versionCode = 69
+        versionCode = 70
         versionName = "0.17.3"
     }
 
