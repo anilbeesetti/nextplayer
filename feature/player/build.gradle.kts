e2a5d7e0895869b5a5a610f32ad1b83fe15bc3ce
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2026-08-01 19:13:41 +0530

    remove constraint layout library

--- feature/player/build.gradle.kts
+++ feature/player/build.gradle.kts
@@ -53,7 +53,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.icons)
-    implementation(libs.androidx.constraintlayout)
 
     implementation(libs.coil.compose)
 
