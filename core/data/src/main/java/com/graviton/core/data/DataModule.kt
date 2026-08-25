afcf725feeed7aa6d3dadd7b815f0ca61bf05f38
Libre <librespark@gmail.com>
2026-08-25 08:42:40 +0530

    Refactor Dagger bindings for process and stream extractors

--- core/data/src/main/java/com/graviton/core/data/DataModule.kt
+++ core/data/src/main/java/com/graviton/core/data/DataModule.kt
@@ -1,25 +1,31 @@
 package com.graviton.core.data
 
+import com.graviton.core.data.repository.LocalMediaRepository
+import com.graviton.core.data.repository.LocalMusicRepository
+import com.graviton.core.data.repository.LocalNetworkConnectionRepository
+import com.graviton.core.data.repository.LocalPlaylistRepository
+import com.graviton.core.data.repository.LocalPreferencesRepository
+import com.graviton.core.data.repository.LocalSearchHistoryRepository
+import com.graviton.core.data.repository.LocalVaultPinRepository
+import com.graviton.core.data.repository.LocalVaultRepository
+import com.graviton.core.data.repository.MediaRepository
+import com.graviton.core.data.repository.MusicRepository
+import com.graviton.core.data.repository.NetworkConnectionRepository
+import com.graviton.core.data.repository.PlaylistRepository
+import com.graviton.core.data.repository.PreferencesRepository
+import com.graviton.core.data.repository.SearchHistoryRepository
+import com.graviton.core.data.repository.VaultPinRepository
+import com.graviton.core.data.repository.VaultRepository
+import com.graviton.core.data.stream.AppYtDlpBinaryLocator
+import com.graviton.core.data.stream.ProcessRunner
+import com.graviton.core.data.stream.StreamExtractor
+import com.graviton.core.data.stream.SystemProcessRunner
+import com.graviton.core.data.stream.YtDlpBinaryLocator
+import com.graviton.core.data.stream.YtDlpStreamExtractor
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import com.graviton.core.data.repository.LocalMediaRepository
-import com.graviton.core.data.repository.LocalNetworkConnectionRepository
-import com.graviton.core.data.repository.LocalPreferencesRepository
-import com.graviton.core.data.repository.LocalPlaylistRepository
-import com.graviton.core.data.repository.LocalSearchHistoryRepository
-import com.graviton.core.data.repository.LocalVaultPinRepository
-import com.graviton.core.data.repository.LocalVaultRepository
-import com.graviton.core.data.repository.LocalMusicRepository
-import com.graviton.core.data.repository.MediaRepository
-import com.graviton.core.data.repository.MusicRepository
-import com.graviton.core.data.repository.NetworkConnectionRepository
-import com.graviton.core.data.repository.PreferencesRepository
-import com.graviton.core.data.repository.PlaylistRepository
-import com.graviton.core.data.repository.SearchHistoryRepository
-import com.graviton.core.data.repository.VaultPinRepository
-import com.graviton.core.data.repository.VaultRepository
 import javax.inject.Singleton
 
 @Module
@@ -75,13 +81,19 @@ interface DataModule {
 
     @Binds
     @Singleton
-    fun bindsProcessRunner(runner: SystemProcessRunner): ProcessRunner
+    fun bindsProcessRunner(
+        runner: SystemProcessRunner,
+    ): ProcessRunner
 
     @Binds
     @Singleton
-    fun bindsYtDlpBinaryLocator(locator: AppYtDlpBinaryLocator): YtDlpBinaryLocator
+    fun bindsYtDlpBinaryLocator(
+        locator: AppYtDlpBinaryLocator,
+    ): YtDlpBinaryLocator
 
     @Binds
     @Singleton
-    fun bindsStreamExtractor(extractor: YtDlpStreamExtractor): StreamExtractor
+    fun bindsStreamExtractor(
+        extractor: YtDlpStreamExtractor,
+    ): StreamExtractor
 }
