e1b933393693c06012648ad42e8d10ad4a5c164f
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2026-09-12 20:29:26 +0530

    Persist external audio attachments for videos

--- core/database/src/main/java/dev/anilbeesetti/nextplayer/core/database/DatabaseModule.kt
+++ core/database/src/main/java/dev/anilbeesetti/nextplayer/core/database/DatabaseModule.kt
@@ -33,6 +33,7 @@ object DatabaseModule {
             MediaDatabase.MIGRATION_8_9,
             MediaDatabase.MIGRATION_9_10,
             MediaDatabase.MIGRATION_10_11,
+            MediaDatabase.MIGRATION_11_12,
         )
         fallbackToDestructiveMigration(false)
     }.build()
--- core/database/src/main/java/dev/anilbeesetti/nextplayer/core/database/MediaDatabase.kt
+++ core/database/src/main/java/dev/anilbeesetti/nextplayer/core/database/MediaDatabase.kt
@@ -22,7 +22,7 @@ import dev.anilbeesetti.nextplayer.core.database.entities.PlaylistItemEntity
         PlaylistEntity::class,
         PlaylistItemEntity::class,
     ],
-    version = 11,
+    version = 12,
     exportSchema = true,
 )
 abstract class MediaDatabase : RoomDatabase() {
@@ -315,6 +315,12 @@ abstract class MediaDatabase : RoomDatabase() {
                     "ALTER TABLE `playlist_item` ADD COLUMN `duration` INTEGER NOT NULL DEFAULT -1",
                 )
                 db.execSQL("ALTER TABLE `playlist_item` ADD COLUMN `group_title` TEXT")
+            }
+        }
+
+        val MIGRATION_11_12 = object : Migration(11, 12) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE `media_state` ADD COLUMN `external_audio` TEXT NOT NULL DEFAULT ''")
             }
         }
 
