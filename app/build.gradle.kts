6938cc5455ce94e1d17c34c8755ccd5c0f403cef
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2026-09-12 10:06:07 +0530

    Bump versionCode to 73 and versionName to 0.18.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         applicationId = "dev.anilbeesetti.nextplayer"
-        versionCode = 72
-        versionName = "0.17.5"
+        versionCode = 73
+        versionName = "0.18.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
