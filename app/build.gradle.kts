70f775318e3d20731617d548b8a1686780c9073a
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2026-09-13 10:39:21 +0530

    Increment versionCode from 73 to 74

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         applicationId = "dev.anilbeesetti.nextplayer"
-        versionCode = 73
+        versionCode = 74
         versionName = "0.18.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
